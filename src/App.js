d6b75de4f61c1ebbabd5dcef6ac1580dceaaaa66
thlanza <thlanza@hotmail.com>
2019-10-21 11:22:55 -0300

    código para pegar adados do firestore e hoc withSpinner

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ class App extends Component {
 
   componentDidMount() {
     const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
      if (userAuth) {
        const userRef = await createUserProfileDocument(userAuth);
@@ -33,7 +34,7 @@ class App extends Component {
        });
      } else {
      setCurrentUser(userAuth);
-     }
+     };
     })
   }
 
@@ -58,7 +59,7 @@ class App extends Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
 })
 
 const mapDispatchToProps = dispatch => ({
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../../components/collection-preview/collection-preview.component'
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 import { Route } from 'react-router-dom';
 import CollectionPage from '../collection/collection.component';
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils'
+import { connect } from 'react-redux';
+import { updateCollections } from '../../redux/shop/shop.actions';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
-const ShopPage = ({ match }) => {
-    console.log(match);
-    return (
-    <div className='shop-page'>
-         <Route exact path={`${match.path}`} component={CollectionsOverview} />
-         <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-     </div> 
-    )
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
+class ShopPage extends Component {   
+  state = {
+        loading: true
+    }
+
+  unsubscribeFromSnapshot = null;
+
+   componentDidMount() {
+      const { updateCollections } = this.props;
+      const collectionRef = firestore.collection('collections');
+    
+      collectionRef.get().then(snapshot => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        updateCollections(collectionsMap);
+        this.setState({ loading: false });
+      })
+    }
+
+   render() {
+       const { match } = this.props;
+       const { loading } = this.state;
+       return (
+        <div className='shop-page'>
+        <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props} /> }/>
+        <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props} /> } />
+    </div> 
+       )
+   }
 }
 
-export default ShopPage;
+const mapDispatchToProps = dispatch => ({
+    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+})
+
+
+export default connect(null, mapDispatchToProps)(ShopPage);
